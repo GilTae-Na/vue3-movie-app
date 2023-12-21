601b0091901ac31773c2872365fa2e85fb72e19f
nagiltae <nagt1997@gmail.com>
2023-12-21 17:22:36 +0900

    Header - Logo 및 Google Fonts

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,6 @@
 <template>
     <header>
+        <Logo/>
         <div class="nav nav-pills">
             <div 
                 v-for="nav in navigations"
@@ -17,11 +18,16 @@
 </template>
 
 <script>
+import Logo from './Logo.vue';
+
 
 export default{
+    components: { 
+        Logo 
+    },
     data() {
         return {
-            navigations:[
+            navigations: [
                 {
                     name: 'Search',
                     href: '/'
@@ -35,7 +41,19 @@ export default{
                     href: '/about'
                 }
             ]
-        }
+        };
     }
 }
 </script>
+
+<style lang="scss" scoped>
+header {
+  height: 70px;
+  padding: 0 40px;
+  display: flex;
+  align-items: center;
+  .logo {
+    margin-right: 40px;
+  }
+}
+ </style>
